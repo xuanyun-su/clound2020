0ad30c44300928b4a3e1686d5c3519aa4b8b2c32
suli <suli249221@163.com>
2022-04-30 11:02:48 +0800

    consul 8500 over

--- cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/controller/OrderZKController.java
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/controller/OrderZKController.java
@@ -11,7 +11,7 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 public class OrderZKController {
-    public static final String INVOKE_URL = "http://cloud-provider-payment";
+    public static final String INVOKE_URL = "http://consul-provider-payment";
 
     @Resource
     private RestTemplate restTemplate;
