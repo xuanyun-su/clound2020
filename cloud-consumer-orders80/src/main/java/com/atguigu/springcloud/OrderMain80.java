168f03e835dc1f7e05247575707bb802fce9d775
suli <suli249221@163.com>
2022-05-04 14:27:01 +0800

    手写rabbion负载均衡完毕

--- cloud-consumer-orders80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-orders80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -1,9 +1,11 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.myrule.MySelfRule;
 import com.sun.org.apache.xpath.internal.operations.Or;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * Hello world!
@@ -12,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="CLOUD-PAYMENT-SERVICE",configuration= MySelfRule.class)
 public class OrderMain80
 {
     public static void main(String[] args) {
--- cloud-consumer-orders80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-orders80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,7 +3,11 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class OrderController {
-
+    @Resource
+    private DiscoveryClient discoveryClient;
     @GetMapping("/hello")
     public String nihao(){
         return "nihao";
@@ -24,6 +31,9 @@ public class OrderController {
     public static final String PAYMENT_URL ="http://CLOUD-PAYMENT-SERVICE";
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private LoadBalancer loadBalancer;
 
 
     @GetMapping("/consumer/payment/create")
@@ -47,6 +57,17 @@ public class OrderController {
         }else{
             return new CommonResult<>(444,"操作失败");
         }
+    }
+
+    @GetMapping(value = "/consumer/payment/lb")
+    public String getPaymentLB(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if(instances == null || instances.size() < 0){
+            return  null;
+        }
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+        return restTemplate.getForObject(uri+"/payment/lb",String.class);
     }
 
 
