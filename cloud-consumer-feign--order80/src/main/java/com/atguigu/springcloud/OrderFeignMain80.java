09381fa83d79072037ffc77473660bee59dfbbbf
suli <suli249221@163.com>
2022-05-05 14:33:38 +0800

    openFeign over

---
+++ cloud-consumer-feign--order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrderFeignMain80.class,args);
+    }
+}
+
+
+
