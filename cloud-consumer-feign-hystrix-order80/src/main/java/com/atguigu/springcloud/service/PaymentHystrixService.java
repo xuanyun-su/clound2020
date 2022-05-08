a8c3f30be9b15a8abec130f6ca6e1ffdef62027d
suli <suli249221@163.com>
2022-05-08 22:06:52 +0800

    服务降级初体验

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService
+{
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public abstract String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public abstract String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+}
+
